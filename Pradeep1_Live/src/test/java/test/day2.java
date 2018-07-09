7c39ae58a479a18c2498f295d6ad19a37a1230e6
pradeep boolchandani <pradeepboolchandani81@gmail.com>
2018-07-09 23:18:53 +0530

    day2 updated

--- Pradeep1_Live/src/test/java/test/day2.java
+++ Pradeep1_Live/src/test/java/test/day2.java
@@ -4,16 +4,21 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class day2 {
   public WebDriver driver;
+  public WebDriverWait wait;
 	@Test(priority=0)
   public void test() {
 		System.out.println("Test started");
 		System.setProperty("webdriver.chrome.driver", "C:\\Latest drivers servers\\chromedriver.exe");
 		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, 10);
 		driver.navigate().to("http://www.facebook.com");
+		wait.until(ExpectedConditions.titleIs(driver.getTitle()));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		System.out.println("The current  url is " +driver.getCurrentUrl());
