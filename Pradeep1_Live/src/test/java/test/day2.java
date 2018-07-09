822b0472402122c3a47eb13f0388dea336020afc
pradeep boolchandani <pradeepboolchandani81@gmail.com>
2018-07-09 22:39:18 +0530

    day2.java commit

---
+++ Pradeep1_Live/src/test/java/test/day2.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class day2 {
+  public WebDriver driver;
+	@Test
+  public void test() {
+		System.out.println("Test started");
+		System.setProperty("webdriver.chrome.driver", "C:\\Latest drivers servers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("http://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		System.out.println("The current  url is " +driver.getCurrentUrl());
+		System.out.println("The title is " +driver.getTitle());
+		System.out.println("Test completed");
+		
+  }
+}
